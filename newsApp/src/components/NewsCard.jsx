71f100eaf453ab5bfffac7bb43cd61351b83e4d6
karimliaghabala <agabala.93@gmail.com>
2025-11-27 04:49:21 +0100

    updTE

--- newsApp/src/components/NewsCard.jsx
+++ newsApp/src/components/NewsCard.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import { Link } from "lucide-react";
+import React from "react";
 
-const NewsCard = ({article}) => {
-    const {source,author, title , description, url, image_url} = article
+const NewsCard = ({ article }) => {
+  const { source, source_name, title, description, link, image_url } = article;
   return (
-    <div className='max-w-md mx-auto bg-white dark:bg-gray-900 hover:scale-105 transition-all rounded-xl shadow-md overflow-hidden hover:shadow-lg duration-300'>
-      <img src={image_url} alt={title} className='w-full h-48 object-cover bg-gray-400' />
+    <div className="max-w-md mx-auto bg-white dark:bg-gray-900 hover:scale-105 transition-all rounded-xl shadow-md overflow-hidden hover:shadow-lg duration-300">
+      <img
+        src={image_url}
+        alt={title}
+        className="w-full h-48 object-cover bg-gray-400"
+      />
+      <div className="p-4">
+        <Link to={link}>
+          <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200 hover:text-blue-600 transition">
+            {description}
+          </h2>
+        </Link>
+        <p>
+          {title?.lenght > 100
+            ? title.slice(0, 100) + "..."
+            : title}
+        </p>
+        <div className="mt-4 text-sm text-gray-500 dark:text-gray-400">
+            <span>By {source_name||"Adsız"}</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewsCard
+export default NewsCard;
